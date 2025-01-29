a426b16c10d4e4d70ff7d58700517286c6070d07
tanvikubade17 <kubadetanvi02@gmail.com>
2025-01-29 09:45:24 +0530

    Batch and Video Repo
    Batch: services, controller, repository.
    Video: services, controller, repository.

---
+++ ComputerSeekho/src/main/java/com/Project/Services/BatchServiceImpl.java
@@ -0,0 +1,43 @@
+package com.Project.Services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.Project.Entities.Batch;
+import com.Project.Repositories.BatchRepository;
+
+@Service
+public class BatchServiceImpl implements BatchService{
+
+    @Autowired
+    BatchRepository batchRepository;
+
+    @Override
+    public void addBatch(Batch b) {
+        batchRepository.save(b);
+    }
+
+    @Override
+    public List<Batch> getAllBatches() {
+        return batchRepository.findAll();
+    }
+
+    @Override
+    public Optional<Batch> getByBatchName(String batch_name) {
+        return batchRepository.findByBatchName(batch_name);
+    }
+
+    @Override
+    public void delete(int batch_id) {
+        batchRepository.deleteById(batch_id);
+    }
+
+    @Override
+    public void activateBatch(int batch_id,Boolean batch_is_active) {
+        batchRepository.activateBatch(batch_is_active,batch_id);
+    }
+    
+}
