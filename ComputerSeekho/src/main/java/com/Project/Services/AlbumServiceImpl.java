09dacbfd891a261960db6b89170a70156dc09508
Borane Nikhil Vijay <57571412+NIKHILBz@users.noreply.github.com>
2025-01-29 11:55:31 +0530

    Album controller created
    Options:
    Add
    GetAll
    Delete
    Update
    GetById

---
+++ ComputerSeekho/src/main/java/com/Project/Services/AlbumServiceImpl.java
@@ -0,0 +1,40 @@
+package com.Project.Services;
+
+import com.Project.Entities.Album;
+import com.Project.Repositories.AlbumRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class AlbumServiceImpl implements AlbumService {
+
+    @Autowired
+    private AlbumRepository albumRepository;
+
+    @Override
+    public Album saveAlbum(Album album) {
+        return albumRepository.save(album);
+    }
+
+    @Override
+    public List<Album> getAllAlbums() {
+        return albumRepository.findAll();
+    }
+
+    @Override
+    public Album getAlbumById(int albumId) {
+        return albumRepository.findById(albumId).orElse(null);
+    }
+
+    @Override
+    public Album updateAlbum(Album album) {
+        return albumRepository.save(album);
+    }
+
+    @Override
+    public void deleteAlbum(int albumId) {
+        albumRepository.deleteById(albumId);
+    }
+}
