1fcafe50f41e5050e17639ba6f3b4c106f47cd83
Tushar483 <bhadriketushar7@gmail.com>
2025-02-07 18:37:45 +0530

    Fit FrontEnd

--- Computer-Seekho-Client-App/src/App.jsx
+++ Computer-Seekho-Client-App/src/App.jsx
@@ -1,33 +1,41 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './index.css';
+import LandPage from './components/Assemble/LandPage';
+import { createBrowserRouter, RouterProvider,} from "react-router-dom";;
+
+import Gallery from './components/Gallery/Gallery';
+import AssembleAbout from './components/AboutUs/AssembleAbout';
+import AboutUs from './components/AboutUs/AboutUs'
+import AssembledCourse from './components/MainCourse/AssembledCourse';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <LandPage/>
+  },
+  {
+    path: "/About",
+    // element: <AssembleAbout/>
+    element:<AboutUs/>
+  },
+  {
+    path:"/Gallery",
+    element : <Gallery/>
+   
+  },
+  {
+    path:"/CourseDetails",
+    element : <AssembledCourse/>
+   
+  }
+]);
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <RouterProvider router={router} />
     </>
   )
 }
---
+++ Computer-Seekho-Client-App/src/components/Text/Name.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Name.css"; // Import the CSS file
+
+const Name = () => {
+  return (
+    <div className="heading-container">
+      {/* Heading Section */}
+      <div className="heading-content">
+        <i className="heading-icon"></i> {/* Icon Placeholder */}
+        <span className="heading-text">
+          USM's Shriram Mantri Vidyanidhi Info Tech Academy
+        </span>
+      </div>
+    </div>
+  );
+};
+
+export default Name;
