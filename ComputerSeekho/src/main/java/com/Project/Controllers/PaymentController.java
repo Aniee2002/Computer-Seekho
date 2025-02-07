3b3e09ec6c712fcbcc8e62c370e6bec0984f8c4c
Borane Nikhil Vijay <57571412+NIKHILBz@users.noreply.github.com>
2025-02-07 12:22:18 +0530

    Update PaymentController.java

--- ComputerSeekho/src/main/java/com/Project/Controllers/PaymentController.java
+++ ComputerSeekho/src/main/java/com/Project/Controllers/PaymentController.java
@@ -36,29 +36,34 @@ public class PaymentController {
         return paymentService.savePayment(payment);
     }
 
-    @PutMapping("/{id}")
-    public ResponseEntity<Payment> updatePayment(@PathVariable int id, @RequestBody Payment paymentDetails) {
-        Payment payment = paymentService.getPaymentById(id);
-        if (payment != null) {
-            payment.setPaymentTypeId(paymentDetails.getPaymentTypeId());
-            payment.setPaymentDate(paymentDetails.getPaymentDate());
-            payment.setStudent(paymentDetails.getStudent());
-            payment.setAmount(paymentDetails.getAmount());
-            Payment updatedPayment = paymentService.updatePayment(payment);
-            return ResponseEntity.ok(updatedPayment);
-        } else {
-            return ResponseEntity.notFound().build();
-        }
-    }
+    // @PutMapping("/{id}")
+    // public ResponseEntity<Payment> updatePayment(@PathVariable int id, @RequestBody Payment paymentDetails) {
+    //     Payment payment = paymentService.getPaymentById(id);
+    //     if (payment != null) {
+    //         payment.setPaymentTypeId(paymentDetails.getPaymentTypeId());
+    //         payment.setPaymentDate(paymentDetails.getPaymentDate());
+    //         payment.setStudent(paymentDetails.getStudent());
+    //         payment.setAmount(paymentDetails.getAmount());
+    //         Payment updatedPayment = paymentService.updatePayment(payment);
+    //         return ResponseEntity.ok(updatedPayment);
+    //     } else {
+    //         return ResponseEntity.notFound().build();
+    //     }
+    // }
 
-    @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deletePayment(@PathVariable int id) {
-        Payment payment = paymentService.getPaymentById(id);
-        if (payment != null) {
-            paymentService.deletePayment(id);
-            return ResponseEntity.noContent().build();
-        } else {
-            return ResponseEntity.notFound().build();
-        }
-    }
+    // @DeleteMapping("/{id}")
+    // public ResponseEntity<Void> deletePayment(@PathVariable int id) {
+    //     Payment payment = paymentService.getPaymentById(id);
+    //     if (payment != null) {
+    //         paymentService.deletePayment(id);
+    //         return ResponseEntity.noContent().build();
+    //     } else {
+    //         return ResponseEntity.notFound().build();
+    //     }
+    // }
+
+
+
+    //as we are not using update and delete payment methods in this project
+    //we have commented them out
 }
