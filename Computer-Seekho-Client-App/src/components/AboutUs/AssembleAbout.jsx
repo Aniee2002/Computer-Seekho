dc716fce3dd69f5978a4cf4118332f29ca45f511
Tushar483 <bhadriketushar7@gmail.com>
2025-02-09 16:45:11 +0530

    gallery fetch aboutus changes
    fit gallery fetch & aboutus Cards

--- Computer-Seekho-Client-App/src/components/AboutUs/AssembleAbout.jsx
+++ Computer-Seekho-Client-App/src/components/AboutUs/AssembleAbout.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Origin from './Origin';
 import Staff from './AboutUs';
 import Navbar from '../Navbar/Navbar';
+import CardsForMoreValue from './CardsForMoreValue';
 
 const AssembleAbout = () => {
   return (
    <>
       <Navbar/>
       <Origin/>
+      <CardsForMoreValue/>
       <Staff/>
     </>
   )
