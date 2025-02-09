c1799c6f3c079066bebb48c008fa6fe8f8d633bf
Anurag Patil <anuragapatil2002@gmail.com>
2025-02-09 16:01:54 +0530

    Batch & ContactUs Page added

--- Computer-Seekho-Client-App/src/components/Batch/BatchWiseStudent.jsx
+++ Computer-Seekho-Client-App/src/components/Batch/BatchWiseStudent.jsx
@@ -1,23 +1,25 @@
 import React, { useState, useEffect } from "react";
-import {useParams} from "react-router-dom"
+import { useParams } from "react-router-dom"
 import './Batch.css';
+import Navbar from "../Navbar/Navbar";
+import Footer from "../Footer/Footer";
 
 const Placedstudent = () => {
     const [PlacedstudentList, setPlacedstudentList] = useState([]);
     const { batchId } = useParams();
     console.log(batchId);
-    
-      // Fetch staff data from the backend
-     useEffect(()=>{
-        const fetchPlacedstudent = async() =>{
-            try{
+
+    // Fetch staff data from the backend
+    useEffect(() => {
+        const fetchPlacedstudent = async () => {
+            try {
                 const response = await fetch(`http://localhost:8080/placement/getByBatch/${batchId}`,{
                     method: "GET",
                     headers: {
                         "Content-Type": "application/json"
                     }
                 });
-                if(!response.ok) {
+                if (!response.ok) {
                     throw new Error("Failed to Fetch");
                 }
                 const data = await response.json();
@@ -27,28 +29,32 @@ const Placedstudent = () => {
                     company: Placedstudent.recruiterName
                 }));
                 setPlacedstudentList(formattedStudents);
-            }catch(error){
+            } catch (error) {
                 console.error("Error Fetching Data", error);
             }
         };
-    
+
         fetchPlacedstudent();
-     }, [])
-  return (
-    <div className="wrapper">
-        {/* <Navbar/> */}
-      {PlacedstudentList.map((student,index) => (
-        <div className="card">
-          <img src = {student.img} alt="Staff1"/>
-          <div className="info">
-            <h1>{student.name}</h1>
-            <p>{student.company}</p>
-            {/* <a href='#' className="btn">Read...</a> */}
-          </div>
+    }, [])
+    return (
+        <div>
+            <Navbar />
+            <div className="wrapper">
+                {/* <Navbar/> */}
+                {PlacedstudentList.map((student, index) => (
+                    <div className="card">
+                        <img src={student.img} alt="Staff1" />
+                        <div className="info">
+                            <h1>{student.name}</h1>
+                            <p>{student.company}</p>
+                            {/* <a href='#' className="btn">Read...</a> */}
+                        </div>
+                    </div>
+                ))}
+            </div>
+            <Footer />
         </div>
-      ))}
-      </div>
-  );
+    );
 }
 
 export default Placedstudent;
