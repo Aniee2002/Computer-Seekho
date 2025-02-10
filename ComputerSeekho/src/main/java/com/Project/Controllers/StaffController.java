06a4579219bbdb8865de5c436987ec414202116e
shivamgadakh <101393684+shivamgadakh@users.noreply.github.com>
2025-02-10 13:12:02 +0530

    staff

--- ComputerSeekho/src/main/java/com/Project/Controllers/StaffController.java
+++ ComputerSeekho/src/main/java/com/Project/Controllers/StaffController.java
@@ -17,12 +17,14 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 
 
 
 
+@CrossOrigin(origins = "http://localhost:5173")  
 @RestController
 @RequestMapping("/staff")
 public class StaffController {
